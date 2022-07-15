9c50757236ebe156aa22fa53fd9cd5e57c3aff02
abdoo431 <34003275+abdoo431@users.noreply.github.com>
2022-07-15 03:34:32 +0200

    add  Employees Controller

---
+++ FactoryDesignParrern/DAL/AppDBContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using FactoryDesignParrern.Models;
+
+#nullable disable
+
+namespace FactoryDesignParrern.DAL
+{
+    public partial class AppDBContext : DbContext
+    {
+        public AppDBContext()
+        {
+        }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Employee> Employees { get; set; }
+        public virtual DbSet<EmployeeType> EmployeeTypes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseSqlServer("Server=HOME-PC\\AHK;Database=FactoryDesignParrernDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employee");
+
+                entity.Property(e => e.Bonus).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.Department).HasMaxLength(50);
+
+                entity.Property(e => e.Hourlypay).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.JobDescription).HasMaxLength(50);
+
+                entity.Property(e => e.Name).HasMaxLength(50);
+
+                entity.Property(e => e.Number).HasMaxLength(50);
+
+                entity.HasOne(d => d.EmployeeType)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.EmployeeTypeId)
+                    .HasConstraintName("FK_Employee_Employee_Type");
+            });
+
+            modelBuilder.Entity<EmployeeType>(entity =>
+            {
+                entity.ToTable("Employee_Type");
+
+                entity.Property(e => e.EmployeeType1)
+                    .HasMaxLength(50)
+                    .HasColumnName("EmployeeType");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ FactoryDesignParrern/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace FactoryDesignParrern.Models
+{
+    public partial class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string JobDescription { get; set; }
+        public string Number { get; set; }
+        public string Department { get; set; }
+        public decimal? Hourlypay { get; set; }
+        public decimal? Bonus { get; set; }
+        public int? EmployeeTypeId { get; set; }
+
+        public virtual EmployeeType EmployeeType { get; set; }
+    }
+}
